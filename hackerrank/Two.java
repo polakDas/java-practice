db67b439f162961d09a65772b87c3c0d2cade597
polakD <pulok3968@gmail.com>
2023-07-28 21:56:30 +0600

    start hackerrank

---
+++ hackerrank/Two.java
@@ -0,0 +1,23 @@
+// user input
+
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Two {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // get number
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        // print number
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        scan.close();
+
+    }
+}
